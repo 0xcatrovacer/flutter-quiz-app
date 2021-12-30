e7a7b677b5a71ec911052385cf94bf64efdea71d
Swarnab Garang <swarnabgarang@gmail.com>
2021-12-30 21:10:44 +0530

    added scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer Chosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favourite animal?",
+      "What is your favourite colour?"
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My First App')),
-        body: Text("This is Body Text"),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(onPressed: answerQuestion, child: Text("Answer 1")),
+            ElevatedButton(onPressed: answerQuestion, child: Text("Answer 2")),
+            ElevatedButton(onPressed: answerQuestion, child: Text("Answer 3")),
+          ],
+        ),
       ),
     );
   }
