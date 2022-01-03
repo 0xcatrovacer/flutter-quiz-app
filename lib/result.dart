b7446884ab0b724e5b07707e4181f8c58a4b9845
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-03 21:45:06 +0530

    added dynamic results

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 10) {
+      resultText = "You don't have any friends";
+    } else if (resultScore <= 20) {
+      resultText = "You want to have friends but have very few.";
+    } else if (resultScore <= 30) {
+      resultText = "You are pretty likeable among friends.";
+    } else {
+      resultText = "You are very popular among friends";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Congratulations! You Completed the Quiz"),
+      child: Text(
+        resultPhrase,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
