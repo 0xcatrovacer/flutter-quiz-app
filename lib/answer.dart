f457ed7eb320dce6e58a0f9180fd877e71720f00
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-03 20:02:57 +0530

    mapped list to widgets to render dynamic options

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: ElevatedButton(onPressed: selectHandler, child: Text("Answer 1")),
+      child: ElevatedButton(onPressed: selectHandler, child: Text(answerText)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -27,19 +27,26 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      "What is your favourite animal?",
-      "What is your favourite colour?"
+      {
+        'questionText': "What is your favourite colour?",
+        'answers': ['Black', "Red", "Green", "White"]
+      },
+      {
+        'questionText': "What is your favourite animal?",
+        'answers': ['Rabbit', "Hawk", "Snake", "Elephant"]
+      }
     ];
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('My First App')),
+        appBar: AppBar(title: Text('Quiz App')),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion)
+            Question(questions[_questionIndex]['questionText'] as String),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
