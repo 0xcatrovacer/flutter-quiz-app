6942d966a301db6c2aa065689f59a95465be90f9
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-03 21:33:32 +0530

    added scorekeeping

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
       print("Answer Chosen!");
@@ -29,19 +32,39 @@ class _MyAppState extends State<MyApp> {
     final _questions = [
       {
         'questionText': "What is your favourite colour?",
-        'answers': ['Black', "Red", "Green", "White"]
+        'answers': [
+          {"text": "Black", "score": 10},
+          {"text": "Red", "score": 1},
+          {"text": "Green", "score": 7},
+          {"text": "White", "score": 4},
+        ]
       },
       {
         'questionText': "What is your favourite animal?",
-        'answers': ['Rabbit', "Hawk", "Snake", "Elephant"]
+        'answers': [
+          {"text": "Rabbit", "score": 4},
+          {"text": "Snake", "score": 1},
+          {"text": "Elephant", "score": 7},
+          {"text": "Hawk", "score": 10},
+        ]
       },
       {
-        'questionText': "What is your favourite food?",
-        'answers': ["Pizza", "Pasta", "Sandwich"]
+        'questionText': "What is your favourite beverage?",
+        'answers': [
+          {"text": "Coke", "score": 7},
+          {"text": "Juice", "score": 4},
+          {"text": "Tea", "score": 1},
+          {"text": "Coffee", "score": 10},
+        ]
       },
       {
         'questionText': "What is your hobby?",
-        'answers': ["Reading", "Exercising", "Writing", "Singing"]
+        'answers': [
+          {"text": "Reading", "score": 10},
+          {"text": "Writing", "score": 7},
+          {"text": "Exercising", "score": 4},
+          {"text": "Singing", "score": 1},
+        ]
       },
     ];
 
