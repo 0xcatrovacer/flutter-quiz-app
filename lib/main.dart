418b1c1d2ebd2bc1505fbc19e851c9b4f549bec5
Swarnab Garang <swarnabgarang@gmail.com>
2022-01-03 20:56:12 +0530

    handled overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -48,15 +48,19 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Quiz App')),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText'] as String),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText'] as String),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text("Congratulations! You Completed the Quiz"),
+              ),
       ),
     );
   }
